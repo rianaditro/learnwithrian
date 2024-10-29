b9625586ed5149e1f22801d5ae31317f17bd6fd4
rianaditro <rianaditro@gmail.com>
2024-10-29 15:48:04 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 
-from data import COURSES, pws
+from data import pythonwebscraping_data as pws
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('pythonwebscraping.html', data=pws)
 
-@app.route('/courses/<course_name>/<module_name>')
-def pws_1(course_name, module_name):
-    filepath = f'courses/{course_name}/{module_name}.html'
-    return render_template(filepath)
+@app.route('/courses/<filepath>')
+def open_course(filepath):
+    filepath = f'courses/pythonwebscraping/{filepath}'
+    return render_template(filepath, slides=pws)
 
 
 
--- data.py
+++ data.py
@@ -4,39 +4,128 @@ COURSES = [
         "name": "Python",
         "description": "Learn Python",
         "image": "https://www.python.org/static/opengraph-icon-200x200.png",
+        "filepath": "pythonbasics"
     },
     {
         "id": 2,
         "name": "Basic Web Development",
         "description": "Learn HTML, CSS, and JavaScript",
         "image": "https://www.python.org/static/opengraph-icon-200x200.png",
+        "filepath": "webdevelopment"
     },
     {
         "id": 3,
         "name": "Python Web Scraping",
         "description": "Learn Python for Web Scraping",
         "image": "https://www.python.org/static/opengraph-icon-200x200.png",
-        "filepath": "pythonwebscraping"
+        "filepath": "courses/pythonwebscraping.html"
     },
     {
         "id": 4,
         "name": "Web Development using Flask",
         "description": "Learn Python for Machine Learning",
         "image": "https://www.python.org/static/opengraph-icon-200x200.png",
+        "filepath": "flask"
     },
 ]
 
-pws = [
+pythonwebscraping_data = [
     {
         "id": 1,
         "name": "Python Web Scraping",
-        "description": "Learn Python for Web Scraping",
-        "filepath": "1_data_scraping_introduction.html"
+        "description": "Learn what is web scraping using Python.",
+        "filepath": "1_data_scraping_introduction.html",
+        "image": "static/assets/images/01.png"
     },
     {
         "id": 2,
         "name": "Dealing with String",
-        "description": "String manipulation in Python",
-        "filepath": "2_dealing_with_string.html"
+        "description": "String manipulation to clean data.",
+        "filepath": "2_dealing_with_string.html",
+        "image": "static/assets/images/02.png"
     },
+    {
+        "id": 3,
+        "name": "Python Collections",
+        "description": "Learn how to use Lists and Dictionaries.",
+        "filepath": "3_python_collections.html",
+        "image": "static/assets/images/03.png"
+    },
+    {
+        "id": 4,
+        "name": "Static Web Scraping",
+        "description": "Practice how to scraping a static website.",
+        "filepath": "4_static_web_scraping.html",
+        "image": "static/assets/images/04.png"
+    },
+    {
+        "id": 5,
+        "name": "Data Processing with Pandas",
+        "description": "Learn how to export data in file format.",
+        "filepath": "5_data_processing_with_pandas.html",
+        "image": "static/assets/images/05.png"
+    },
+    {
+        "id": 6,
+        "name": "Scraping Hidden API",
+        "description": "Identify hidden API in a website.",
+        "filepath": "6_scraping_hidden_api.html",
+        "image": "static/assets/images/06.png"
+    },
+    {
+        "id": 7,
+        "name": "Handling Dynamic Content with Selenium",
+        "description": "Hands-on browser automation using selenium.",
+        "filepath": "7_handling_dynamic_content_with_selenium.html",
+        "image": "static/assets/images/07.png"
+    },
+    {
+        "id": 8,
+        "name": "Scraping with Authentication",
+        "description": "Bypass login authentication using Selenium and Requests.",
+        "filepath": "8_scraping_with_authentication.html",
+        "image": "static/assets/images/08.png"
+    },
+    {
+        "id": 9,
+        "name": "Dynamic Web Scraping",
+        "description": "Make your script more flexible using argparse.",
+        "filepath": "9_dynamic_web_scraping.html",
+        "image": "static/assets/images/09.png"
+    },
+    {
+        "id": 10,
+        "name": "Object Oriented Web Scraping",
+        "description": "Organize your code using object oriented programming.",
+        "filepath": "10_object_oriented_scraping.html",
+        "image": "static/assets/images/10.png"
+    },
+    {
+        "id": 11,
+        "name": "Memory Efficient Web Scraping",
+        "description": "Reduce memory usage using generators.",
+        "filepath": "11_memory_efficient_scraping.html",
+        "image": "static/assets/images/11.png"
+    },
+    {
+        "id": 12,
+        "name": "Time Efficient Web Scraping",
+        "description": "Reduce time usage using multithreading and asynchronous.",
+        "filepath": "12_time_efficient_scraping.html",
+        "image": "static/assets/images/12.png"
+    },
+    {
+        "id": 13,
+        "name": "Clean Code & Best Practice",
+        "description": "Writing clean code as a professional.",
+        "filepath": "13_clean_code_best_practice.html",
+        "image": "static/assets/images/13.png"
+    },
+    {
+        "id": 14,
+        "name": "Anti-Scraping Mechanism",
+        "description": "Identify anti-scraping mechanism in a website.",
+        "filepath": "14_anti_scraping_mechanism.html",
+        "image": "static/assets/images/14.png"
+    }
 ]
